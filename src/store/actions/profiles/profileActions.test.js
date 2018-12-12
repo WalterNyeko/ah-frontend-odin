433d5a7a2a6bd8e6030772ebfb8eac528fd7a431
Suzan Nakatudde <sue@MacBook-Pro-11.local>
2018-12-12 10:10:06 +0300

    feat(userprofiles): Users should be able to view and edit their profiles
    - enable users to view their profiles
    - enable users to edit their profiles
    - test the different profile componenets
    
    [Finishes  #161340141]

---
+++ src/store/actions/profiles/profileActions.test.js
@@ -0,0 +1,34 @@
+import mockStore from 'tests/mockStore';
+import types from 'store/types/profiles';
+import { prepareUrl } from 'utils/helpers';
+import { fetchProfile, updateProfile } from './profileActions';
+
+describe('async actions', () => {
+  it('should fetch user profile', () => {
+    let username;
+    const data = {};
+    fetch.get(`https://authors-haven-odin.herokuapp.com/api/profile/${username}`, data);
+    const expectedActions = [{ type: types.FETCH_PROFILE, payload: [] }];
+    const store = mockStore({});
+
+    return store.dispatch(fetchProfile()).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+});
+
+describe('async actions', () => {
+  it('should update user profile', () => {
+    const username = 'username';
+    const data = { bio: 'strong', image: 'hello' };
+    const store = mockStore({});
+    fetch.put(prepareUrl(`profile/${username}`), {
+      status: 201,
+      body: { data },
+    });
+    const expectedActions = [{ type: types.UPDATE_PROFILE, payload: { data } }];
+    return store.dispatch(updateProfile(data, username)).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+});
